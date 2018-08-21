7e4b8004f1840fdedd7f23d2838fa8386ef33f39
rozaliav <rozaliav@DSN2211>
2018-08-21 18:36:35 +0300

    socket tutorial

--- ServerSide.py
+++ ServerSide.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
+import random
+import sys
+import os
+import socket
+
+HOST = '127.0.0.1'    #standard loopback
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(HOST. PORT)
+    s.listen()
+    (conn, address) = s.accept()
+    with conn:
+        print('connected by ', address)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
 
 
-def main():
-    """
-    Add Documentation here
-    """
-    pass  # Replace Pass with Your Code
 
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     """
+#     Add Documentation here
+#     """
+#     pass  # Replace Pass with Your Code
+#
+#
+# if __name__ == '__main__':
+#     main()
